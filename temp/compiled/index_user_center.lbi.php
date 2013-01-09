6c8211489ab9ca0e95421649d727b94b8918a9ce
mjxjn <mjxjn@vip.qq.com>
2013-01-09 17:18:45 +0800

    缓存

--- temp/compiled/index_user_center.lbi.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$k = array (
-  'name' => 'index_user_info',
-);
-echo $this->_echash . $k['name'] . '|' . serialize($k) . $this->_echash;
-?>
--- temp/static_caches/api_str.php
+++
@@ -1,7 +0,0 @@
-<?php
-<<<<<<< HEAD
-$data = '<li style="border: 1px solid #CC0000; background: #FFFFCC; padding: 10px; margin-bottom: 5px;"><div>最新版本: v 2.7.3 released 20121106, 发布日期: 2012-12-27.<a target="_blank" href="http://bbs.ecshop.com/thread-1118680-1-1.html">点击下载</a></div></li>';
-=======
-$data = '';
->>>>>>> c30ca1655a6b15e0b14ec8c4062ab02255db5c4d
-?>
