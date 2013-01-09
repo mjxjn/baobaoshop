66783050409a004002c3d21d0fbcbe1b87fe81a3
mjxjn <mjxjn@vip.qq.com>
2013-01-09 09:03:45 +0800

    缓存文件更新

--- temp/compiled/ur_here.lbi.php
+++
@@ -1,1 +0,0 @@
-您当前的位置：<?php echo $this->_var['ur_here']; ?>
--- temp/static_caches/api_str.php
+++ temp/static_caches/api_str.php
@@ -1,3 +1,3 @@
 <?php
-$data = '<li style="border: 1px solid #CC0000; background: #FFFFCC; padding: 10px; margin-bottom: 5px;"><div>最新版本: v 2.7.3 released 20121106, 发布日期: 2012-11-06.<a target="_blank" href="http://bbs.ecshop.com/thread-1118680-1-1.html">点击下载</a></div></li>';
+$data = '<li style="border: 1px solid #CC0000; background: #FFFFCC; padding: 10px; margin-bottom: 5px;"><div>最新版本: v 2.7.3 released 20121106, 发布日期: 2012-12-27.<a target="_blank" href="http://bbs.ecshop.com/thread-1118680-1-1.html">点击下载</a></div></li>';
 ?>
